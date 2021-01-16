c9a7f7cebe410052e10493f81361109829d4970d
SpeziFisch-DE <67187041+SpeziFisch-DE@users.noreply.github.com>
2021-01-16 19:42:48 +0100

    fetch

--- Kapitel3/Kapitelaufgabe/script.js
+++ Kapitel3/Kapitelaufgabe/script.js
@@ -1,6 +1,7 @@
 "use strict";
 var Kapitelaufgabe3Script;
 (function (Kapitelaufgabe3Script) {
+    let htmlDIV = document.getElementById("div");
     let buttonSubmit = document.getElementById("submit");
     let formData = new FormData(document.forms[0]);
     buttonSubmit.addEventListener("click", handleSubmit);
@@ -8,8 +9,10 @@ var Kapitelaufgabe3Script;
         let url = "https://hfugis2020.herokuapp.com";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
-        await fetch(url);
-        buttonSubmit.innerHTML = "" + formData.get("Name");
+        await fetch(url).then(async function (response) {
+            let responseText = await response.text();
+            console.log(responseText);
+        });
     }
 })(Kapitelaufgabe3Script || (Kapitelaufgabe3Script = {}));
 //# sourceMappingURL=script.js.map
--- Kapitel3/Kapitelaufgabe/script.ts
+++ Kapitel3/Kapitelaufgabe/script.ts
@@ -1,4 +1,5 @@
 namespace Kapitelaufgabe3Script {
+    let htmlDIV: HTMLElement = document.getElementById("div");
     let buttonSubmit: HTMLElement = document.getElementById("submit");
     let formData: FormData = new FormData(document.forms[0]);
 
@@ -8,7 +9,11 @@ namespace Kapitelaufgabe3Script {
         let url: string = "https://hfugis2020.herokuapp.com";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
-        await fetch(url);
-        buttonSubmit.innerHTML = "" + formData.get("Name");
+        await fetch(url).then(async function(response: Response): Promise<void> {
+            let responseText: string = await response.text();
+            console.log(responseText);
+        }
+        );
+
     }
 }
