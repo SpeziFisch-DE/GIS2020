ac3201a97b0a8dd5477c7ce1503fd274d68e3583
SpeziFisch-DE <67187041+SpeziFisch-DE@users.noreply.github.com>
2021-01-17 11:49:16 +0100

    testing

--- Kapitel3/Kapitelaufgabe/script.js
+++ Kapitel3/Kapitelaufgabe/script.js
@@ -1,7 +1,6 @@
 "use strict";
 var Kapitelaufgabe3Script;
 (function (Kapitelaufgabe3Script) {
-    let htmlDIV = document.getElementById("div");
     let buttonSubmit = document.getElementById("submit");
     let formData = new FormData(document.forms[0]);
     buttonSubmit.addEventListener("click", handleSubmit);
@@ -9,6 +8,8 @@ var Kapitelaufgabe3Script;
         let url = "https://hfugis2020.herokuapp.com";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
+        console.log(url);
+        console.log(formData.get("Name"));
         await fetch(url).then(async function (response) {
             let responseText = await response.text();
             console.log(responseText);
--- Kapitel3/Kapitelaufgabe/script.ts
+++ Kapitel3/Kapitelaufgabe/script.ts
@@ -1,5 +1,4 @@
 namespace Kapitelaufgabe3Script {
-    let htmlDIV: HTMLElement = document.getElementById("div");
     let buttonSubmit: HTMLElement = document.getElementById("submit");
     let formData: FormData = new FormData(document.forms[0]);
 
@@ -9,6 +8,8 @@ namespace Kapitelaufgabe3Script {
         let url: string = "https://hfugis2020.herokuapp.com";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
+        console.log(url);
+        console.log(formData.get("Name"));
         await fetch(url).then(async function(response: Response): Promise<void> {
             let responseText: string = await response.text();
             console.log(responseText);
